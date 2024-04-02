2428b7838bc280ceed220ddda51de9673680e4ae
emanuele-pogliari <manu.pogliari@gmail.com>
2024-04-02 16:39:47 +0200

    bonus 2 completed

---
+++ Models/Movies.php
@@ -0,0 +1,35 @@
+<?php
+class Movie
+{
+    public $title;
+    public $originalTitle;
+    public $year;
+    public $length;
+    public $overview;
+
+    /**
+     * __construct
+     *
+     * @param string $_title
+     * @param string $_originalTitle
+     * @param string $_year
+     * @param string $_length
+     * @param string $_overview
+     */
+
+    // constructor function
+    function __construct($_title, $_originalTitle, $_year, $_length, $_overview)
+    {
+        $this->title = $_title;
+        $this->originalTitle = $_originalTitle;
+        $this->year = $_year;
+        $this->length = $_length;
+        $this->overview = $_overview;
+    }
+
+    // method that returns the title of the movie
+    public function getTitle()
+    {
+        return $this->title . ":";
+    }
+}
---
+++ db.php
@@ -0,0 +1,25 @@
+<?php
+
+require './Models/Movies.php';
+
+
+$movie1 = new Movie(
+    "Ritorno al futuro",
+    "Back to the Future",
+    "18/10/1985 ",
+    "1h 56m",
+    "Marty McFly è stato catapultato per errore nel 1955, grazie alla macchina del tempo ideata dal suo amico scienziato Doc. Non avendo più 'carburante' per poter tornare nel futuro si rivolge alla versione più giovane di Doc, che nonostante l'incredulità iniziale si farà in quattro per aiutarlo. Ma nel 1955 non è solo Doc ad essere più giovane, Marty infatti si imbatte casualmente nei suoi genitori, all'epoca teenager, ma l'incontro aggiungerà altri problemi."
+);
+
+$movie2 = new Movie(
+    "Dune - Parte 2",
+    "Dune - Part 2",
+    "28/02/2024",
+    "2h 47m",
+    "Segui il mitico viaggio di Paul Atreides mentre si unisce a Chani e ai Fremen mentre è sul sentiero di guerra per vendicarsi dei cospiratori che hanno distrutto la sua famiglia. Di fronte a una scelta tra l'amore della sua vita e il destino dell'universo conosciuto, Paul si sforza di prevenire un terribile futuro che solo lui può prevedere.",
+);
+
+$movies = [
+    $movie1,
+    $movie2,
+];
